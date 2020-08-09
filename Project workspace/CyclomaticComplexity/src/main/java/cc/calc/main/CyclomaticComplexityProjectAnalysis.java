d9fca5ff1a41b9418e1a4317359c8b9e11abd339
HugoWALTER <hugo.walter@epitech.eu>
2020-08-09 16:06:50 +0200

    push save

--- Project workspace/CyclomaticComplexity/src/main/java/cc/calc/main/CyclomaticComplexityProjectAnalysis.java
+++ Project workspace/CyclomaticComplexity/src/main/java/cc/calc/main/CyclomaticComplexityProjectAnalysis.java
@@ -1,13 +1,23 @@
 package cc.calc.main;
 
+import java.io.File;
+import java.io.IOException;
+
 import se.ck.martin.extensions.Application;
 import se.ck.martin.extensions.Calculator;
 
 public class CyclomaticComplexityProjectAnalysis {
 
-	public static void main(String[] args){
+	public static String getPath() throws IOException {
+		String filePath;
+		filePath = new File("..").getCanonicalPath();
+		filePath = filePath.concat("/Games");
+		return filePath;
+	}
+	
+	public static void main(String[] args) throws IOException {
 		Calculator calc = new Calculator();
-		Application app = calc.run("C:\\Users\\walter_h\\Desktop\\SWE PROJET\\Project workspace\\Games");
+		Application app = calc.run(getPath());
 		System.out.println("++++++++++++ Calculating Cyclomatic Complexity... ++++++++++++");
 		System.out.println();
 		app.printPackageInfo();
